e5f4f25f0f32e2cc14a0aae66610be2cad5056c4
RamandeepSingh2402 <singhramandeep2402@gmail.com>
2022-11-22 21:47:19 -0400

    adding the html, css and js for the geolocator

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
-window.onload();
+const clickMehtod = () => {
+    window.preventDefault();
+    window.onload = (event) => {
+    event.preventDefault();
+    const ip = document.getElementById('realIP');
+    console.log(ip);
+    fetch(`http://ip-api.com/json/${ip}`, {
+        method: 'GET',
+    })
+    .then(res => {
+            console.log(res.json())
+            return res.json();
+        })
+        // .then(data => {
+        //     // document.getElementById('imageOfDay').src= data.hdurl
+        //     console.log(data)
+        // })
+            .catch(error => console.log('ERROR', error))
+    }}
